a33481f1bec4ebe3c46f336f2ae8b8c81d4bbacd
spm507 <57920656+spm507@users.noreply.github.com>
2020-03-24 20:21:28 -0400

    Creation of project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a map displaying the energy consumption of Castle Village and comparing it with similar properties
---
+++ js/scripts.js
@@ -0,0 +1,142 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3BtNTA3IiwiYSI6ImNrNmxmYnZrMTAzNzAzaHBodTRmd2hpZm0ifQ.Tf4_3bYwjcuHv39tq01CWQ';
+
+
+var initialCenterPoint = [-73.9403373, 40.848137]
+var initialZoom = 13
+
+var defaultText = '<p>Move the mouse over the map to see when a property was built </p>'
+$('#feature-info').html(defaultText)
+
+// create an object to hold the initialization options for a mapboxGL map
+var initOptions = {
+  container: 'map-container', // put the map in this container
+  style: 'mapbox://styles/mapbox/dark-v10', // use this basemap
+  center: initialCenterPoint, // initial view center
+  zoom: initialZoom, // initial view zoom level (0-18)
+}
+
+// create the new map
+var map = new mapboxgl.Map(initOptions);
+
+// add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+// wait for the initial style to Load
+map.on('style.load', function() {
+
+  // add a geojson source to the map using our external geojson file
+  map.addSource('Washington_Heights', {
+    type: 'geojson',
+    data: './data/Washington_Heights.geojson',
+  });
+
+  // let's make sure the source got added by logging the current map state to the console
+  console.log(map.getStyle().sources)
+
+  // add a layer for our custom source
+  map.addLayer({
+    id: 'fill-Washington_Heights',
+    type: 'fill',
+    source: 'Washington_Heights',
+    paint: {
+      'fill-color': {
+        property: 'YearBuilt',
+        stops: [
+          [
+            1700,
+            'pink'
+          ],
+          [
+            1800,
+            'orange'
+          ],
+          [
+            1850,
+            'blue'
+          ],
+          [
+            1900,
+            'green'
+          ],
+          [
+            1920,
+            'steelblue'
+          ],
+          [
+            1950,
+            'lavender'
+          ],
+          [
+            1970,
+            'yellow'
+          ],
+          [
+            2000,
+            'turquoise'
+          ],
+
+        ]
+      }
+    }
+  })
+
+  // add an empty data source, which we will use to highlight the lot the user is hovering over
+  map.addSource('highlight-feature', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  })
+
+  // add a layer for the highlighted lot
+  map.addLayer({
+    id: 'highlight-line',
+    type: 'line',
+    source: 'highlight-feature',
+    paint: {
+      'line-width': 2,
+      'line-opacity': 0.9,
+      'line-color': 'white',
+    }
+  });
+
+  // listen for the mouse moving over the map and react when the cursor is over our data
+
+  map.on('mousemove', function (e) {
+    // query for the features under the mouse, but only in the lots layer
+    var features = map.queryRenderedFeatures(e.point, {
+        layers: ['fill-Washington_Heights'],
+    });
+
+    // if the mouse pointer is over a feature on our layer of interest
+    // take the data for that feature and display it in the sidebar
+    if (features.length > 0) {
+      map.getCanvas().style.cursor = 'pointer';  // make the cursor a pointer
+
+      var hoveredFeature = features[0]
+      var featureInfo = `
+        <h4>${hoveredFeature.properties.Address}</h4>
+        <p><strong>Year Built:</strong> ${hoveredFeature.properties.YearBuilt}</p>
+        <p><strong>Zoning:</strong> ${hoveredFeature.properties.ZoneDist1}</p>
+      `
+      $('#feature-info').html(featureInfo)
+
+      // set this lot's polygon feature as the data for the highlight source
+      map.getSource('highlight-feature').setData(hoveredFeature.geometry);
+    } else {
+      // if there is no feature under the mouse, reset things:
+      map.getCanvas().style.cursor = 'default'; // make the cursor default
+
+      // reset the highlight source to an empty featurecollection
+      map.getSource('highlight-feature').setData({
+        type: 'FeatureCollection',
+        features: []
+      });
+
+      // reset the default message
+      $('#feature-info').html(defaultText)
+    }
+  })
+
+})

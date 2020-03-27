d7d821af74d6afc2285cf93340abdc930ea7aac7
spm507 <57920656+spm507@users.noreply.github.com>
2020-03-27 14:23:46 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,6 @@ var initOptions = {
 // create the new map
 var map = new mapboxgl.Map(initOptions);
 
-var features = map.queryRenderedFeatures(e.point, {
- layers: ['fill-castle_village', 'fill-london_terrace', 'fill-boulevard_gardens', 'fill-100_barclay_street']
-});
-
 // add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
@@ -44,8 +40,7 @@ map.on('style.load', function() {
   map.addSource('100_barclay_street', {
     type: 'geojson',
     data: './data/100_barclay_street.geojson',
-  })
-
+  });
 
   // let's make sure the source got added by logging the current map state to the console
   console.log(map.getStyle().sources)
@@ -88,107 +83,27 @@ map.on('style.load', function() {
           })
 
           // Fly to Buttons to each divisions
-          $('#London').on('click', function() {
+  $('#London').on('click', function() {
             map.flyTo({
               center: [-74.0031432, 40.7474312],
               zoom: 16
             });
           });
-          $('#Boulevard').on('click', function() {
+  $('#Boulevard').on('click', function() {
             map.flyTo({
               center: [-73.907924, 40.7588546],
               zoom: 16
             });
           });
-          $('#Barclay').on('click', function() {
+  $('#Barclay').on('click', function() {
             map.flyTo({
               center: [-74.0139115, 40.7139888],
               zoom: 15
             });
           });
-          $('#Castle').on('click', function() {
+  $('#Castle').on('click', function() {
             map.flyTo({
               center: [-73.939862, 40.853433],
               zoom: 16
             });
           });
-
-
-    var popup = new mapboxgl.Popup()
-    // When a click event occurs on a feature in the res layer, open a popup at the
-    // location of the click, with description HTML from its properties.
-    map.on('mouseenter', 'fill-castle_village', function(e) {
-      popup
-      .setLngLat(e.lngLat)
-      .setHTML(e.features[0].properties.PropertyName, e.features[0].properties.YearBuilt)
-      .addTo(map);
-    });
-
-    map.on('mouseenter', 'fill-castle_village', function() {
-    map.getCanvas().style.cursor = 'pointer';
-    });
-
-    map.on('mouseleave', 'fill-castle_village', function() {
-    map.getCanvas().style.cursor = '';
-    popup.remove()
-    });
-
-
-    var popup = new mapboxgl.Popup()
-    // When a click event occurs on a feature in the res layer, open a popup at the
-    // location of the click, with description HTML from its properties.
-    map.on('mouseenter', 'fill-boulevard_gardens', function(e) {
-      popup
-      .setLngLat(e.lngLat)
-      .setHTML(e.features[0].properties.PropertyName, e.features[0].properties.YearBuilt)
-      .addTo(map);
-    });
-
-    map.on('mouseenter', 'fill-boulevard_gardens', function() {
-    map.getCanvas().style.cursor = 'pointer';
-    });
-
-
-    map.on('mouseleave', 'fill-boulevard_gardens', function() {
-    map.getCanvas().style.cursor = '';
-    popup.remove()
-    });
-
-    var popup = new mapboxgl.Popup()
-    // When a click event occurs on a feature in the res layer, open a popup at the
-    // location of the click, with description HTML from its properties.
-    map.on('mouseenter', 'fill-100_barclay_street', function(e) {
-      popup
-      .setLngLat(e.lngLat)
-      .setHTML(e.features[0].properties.PropertyName, e.features[0].properties.YearBuilt)
-      .addTo(map);
-    });
-    // Change the cursor to a pointer when the mouse is over the states layer.
-    map.on('mouseenter', 'fill-100_barclay_street', function() {
-    map.getCanvas().style.cursor = 'pointer';
-    });
-
-    map.on('mouseleave', 'fill-100_barclay_street', function() {
-    map.getCanvas().style.cursor = '';
-    popup.remove()
-    });
-
-    var popup = new mapboxgl.Popup()
-    // When a click event occurs on a feature in the res layer, open a popup at the
-    // location of the click, with description HTML from its properties.
-    map.on('mouseenter', 'fill-london_terrace', function(e) {
-      popup
-      .setLngLat(e.lngLat)
-      .setHTML(e.features[0].properties.PropertyName, e.features[0].properties.YearBuilt)
-      .addTo(map);
-    });
-
-    // Change the cursor to a pointer when the mouse is over the states layer.
-    map.on('mouseenter', 'fill-london_terrace', function() {
-    map.getCanvas().style.cursor = 'pointer';
-    });
-
-    map.on('mouseleave', 'fill-london_terrace', function() {
-    map.getCanvas().style.cursor = '';
-    popup.remove()
-    });
